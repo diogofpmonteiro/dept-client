ca46101b4c1c10f42cf24519b407ec01f94d19bd
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2022-06-07 02:26:17 +0100

    create footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,37 @@
+import "./Footer.css";
+
 const Footer = () => {
-  return <div>Footer to end page</div>;
+  return (
+    <div className='footer-container'>
+      <div className='top-half'>
+        <ul className='footer-list'>
+          <li className='footer-list-title'>Dept</li>
+          <li className='footer-list-top-item'>Work</li>
+          <li className='footer-list-top-item'>Services</li>
+          <li className='footer-list-top-item'>Stories</li>
+          <li className='footer-list-top-item'>About</li>
+          <li className='footer-list-top-item'>Careers</li>
+          <li className='footer-list-top-item'>Contact</li>
+        </ul>
+        <ul className='footer-list'>
+          <li className='footer-list-top-item'>facebook</li>
+          <li className='footer-list-top-item'>twitter</li>
+          <li className='footer-list-top-item'>instagram</li>
+        </ul>
+      </div>
+      <div className='bottom-half'>
+        <ul className='footer-list'>
+          <li className='footer-list-bottom-item'>Chamber of Commerce: 63464101</li>
+          <li className='footer-list-bottom-item'>VAT: NL 8552.47.502.B01</li>
+          <li className='footer-list-bottom-item'>Terms and conditions</li>
+
+          <ul className='footer-list'>
+            <li className='footer-list-bottom-item'>© 2018 Dept Agency</li>
+          </ul>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
