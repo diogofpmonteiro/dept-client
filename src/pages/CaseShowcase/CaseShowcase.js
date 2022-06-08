60c0df287f48c6d3bd4a46985e9d02a63f1f3b5a
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2022-06-08 17:00:48 +0100

    implement filter by selection

--- src/pages/CaseShowcase/CaseShowcase.js
+++ src/pages/CaseShowcase/CaseShowcase.js
@@ -4,69 +4,81 @@ import CaseCard from "../../components/CaseCard/CaseCard";
 
 const CaseShowcase = () => {
   const [casesData, setCasesData] = useState([]);
+  const [filtered, setFiltered] = useState(false);
   const [visibleCases, setVisibleCases] = useState(10);
   const [selectedIndustry, setSelectedIndustry] = useState("");
 
-  const showMoreCases = () => {
-    setVisibleCases((previousValue) => previousValue + 4);
-  };
-
-  const allIndustries = casesData.map((eachCase) => {
+  let allIndustries = casesData.map((eachCase) => {
     return eachCase.industry;
   });
 
   let filteredIndustries = [...new Set(allIndustries)];
 
-  const searchFilter = (event) => {
-    event.preventDefault();
+  // console.log(casesData.filter((eachCase) => eachCase.industry === selectedIndustry));
 
-    console.log(event.target.value);
+  const searchFilter = (event) => {
+    setSelectedIndustry(event.target.value);
+  };
+
+  const showMoreCases = () => {
+    setVisibleCases((previousValue) => previousValue + 4);
   };
 
   useEffect(() => {
     const getCases = async () => {
       const result = await fetch("http://localhost:3000/cases/");
       const data = await result.json();
-      setCasesData(data);
-
       // console.log("fetch data ->", data);
+
+      setCasesData(data);
     };
     getCases();
     setVisibleCases(10);
-  }, []);
+
+    if (selectedIndustry === "All industries" || selectedIndustry === "") {
+      setFiltered(false);
+    } else {
+      setFiltered(true);
+    }
+  }, [selectedIndustry]);
 
   return (
     <div className='case-showcase-container'>
-      {/* // ! use buttons or form with select option? */}
-
       <form method='get' id='filter' className='filter-form'>
         <span className='form-text'>in </span>
-        <select name='industries' form='filter' onSelect={searchFilter} className='select-btn'>
-          <option value=''>All industries</option>
+        <select name='industries' form='filter' value={selectedIndustry} onChange={searchFilter} className='select-btn'>
+          <option>All industries</option>
+
           {filteredIndustries.map((industry) => (
-            <option value=''>{industry}</option>
+            <option key={industry}>{industry}</option>
           ))}
         </select>
         <span className='form-text'>show me</span>
         <select name='industries' form='filter' className='select-btn'>
-          <option value=''>Alphabetically</option>
-          <option value=''>By most recent</option>
+          <option>All work</option>
         </select>
       </form>
 
       <ul className='case-list'>
-        {casesData &&
-          casesData.slice(0, visibleCases).map((eachCase) => (
-            <li className='case-list-item' key={eachCase.id}>
-              <CaseCard eachCase={eachCase} />
-            </li>
-          ))}
+        {casesData && !filtered
+          ? casesData.slice(0, visibleCases).map((eachCase) => (
+              <li className='case-list-item' key={eachCase.id}>
+                <CaseCard eachCase={eachCase} />
+              </li>
+            ))
+          : casesData
+              .filter((eachCase) => eachCase.industry === selectedIndustry)
+              .map((eachCase) => (
+                <li className='case-list-item' key={eachCase.id}>
+                  <CaseCard eachCase={eachCase} />
+                </li>
+              ))}
+        {visibleCases <= casesData.length ? (
+          <button className='loadmore-btn' onClick={showMoreCases}>
+            Load More
+          </button>
+        ) : null}
       </ul>
-      {visibleCases <= casesData.length ? (
-        <button className='loadmore-btn' onClick={showMoreCases}>
-          Load More
-        </button>
-      ) : null}
     </div>
   );
 };
